b540c6e353251099a51cdccacbec55447185c66c
liansun96 <liansun0203@gmail.com>
2025-04-21 15:21:28 +0630

    update server

---
+++ Server/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const singup = async(req ,res) => {
+    res.send('Sign Up Route')
+}
+
+export const login = async(req , res) => {
+    res.send('Login Route')
+}
+
+export const logout = async(req , res) => {
+    res.send('Logout Route')
+}
+ 
--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,14 @@
-const express = require('express')
+import express from 'express'
 const app = express()
+import authRoute from './routes/auth.route.js'
+
+app.use('/api/auth' , authRoute) 
 
 const port = 3001
+
 
 const start = async () => {
     app.listen(port , console.log(`Server is listening on ${port}`))
 }
 
-start()
+start()  
