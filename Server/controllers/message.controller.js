3765eba6aad747f566d027c0ce98f13ebd26dbb5
liansun96 <liansun0203@gmail.com>
2025-05-08 22:09:36 +0630

    update message controller

--- Server/controllers/message.controller.js
+++ Server/controllers/message.controller.js
@@ -34,32 +34,32 @@ export const getMessages = async(req, res) => {
     }
 }
 
-export const sendMessage = async(req, res) => {
-    try {
-        const {text , image} = req.body
-        const {id : receiverId } = req.params
-        const senderId = req.user._id
+// export const sendMessage = async(req, res) => {
+//     try {
+//         const {text , image} = req.body
+//         const {id : receiverId } = req.params
+//         const senderId = req.user._id
  
-        let imageUrl;
-        if(image){
-            const uploadResponse = await cloudinary.uploader.upload(image)
-            imageUrl = uploadResponse.secure_url
-        }
+//         let imageUrl;
+//         if(image){
+//             const uploadResponse = await cloudinary.uploader.upload(image)
+//             imageUrl = uploadResponse.secure_url
+//         }
 
-        const newMessage = new Message({
-            senderId , 
-            receiverId,
-            text,
-            image : imageUrl
-        })
+//         const newMessage = new Message({
+//             senderId , 
+//             receiverId,
+//             text,
+//             image : imageUrl
+//         })
 
-        await newMessage.save()
+//         await newMessage.save()
 
-        //realtime functionality goes here => socket.io
+//         //realtime functionality goes here => socket.io
 
-        res.status(201).json(newMessage)
-    } catch (error) {
-        console.error('Error in sendMessage controller' , error.message)
-        res.status(500).json({error : "Internal Server Error"})
-    }
-}
+//         res.status(201).json(newMessage)
+//     } catch (error) {
+//         console.error('Error in sendMessage controller' , error.message)
+//         res.status(500).json({error : "Internal Server Error"})
+//     }
+// }
