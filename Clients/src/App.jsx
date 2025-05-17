fb0a20607fca016e5bff49ec07743085c125a515
liansun96 <liansun0203@gmail.com>
2025-05-17 23:30:15 +0630

    update

--- Clients/src/App.jsx
+++ Clients/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import SIgnUpPage from './pages/SIgnUpPage'
 
 const App = () => {
   return (
       <div>
         <Navbar/>
+        <SIgnUpPage/>
       </div>
   )
 }
