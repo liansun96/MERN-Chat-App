ae3077f50919c7446a19cf081e570e0abfe5fb8b
liansun96 <liansun0203@gmail.com>
2025-05-20 19:01:14 +0630

    Implementing reusable authentication state hooks across componentsand loading indicators in a React app.

--- Clients/src/App.jsx
+++ Clients/src/App.jsx
@@ -1,37 +1,43 @@
 import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import SignUpPage from './pages/SIgnUpPage'
-import {Routes , Route} from 'react-router-dom'
+import {Routes , Route, Navigate} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LogInPage from './pages/LogInPage'
 import ProfilePage from './pages/ProfilePage'
 import SettingPage from './pages/SettingPage'
 import { useAuthStore } from './store/useAuthStore'
+import {Loader} from 'lucide-react'
 
 const App = () => {
 
-  const {authUser , checkAuth} = useAuthStore()
+  const {authUser , checkAuth , isCheckingAuth} = useAuthStore()
 
   useEffect(() => {   
       checkAuth()
   } , [authUser])
 
   console.log({authUser});
-  
+
+  if(isCheckingAuth && !authUser) return (
+    <div className='flex items-center justify-center h-screen'>
+        <Loader className='size-10 animate-spin'/>
+    </div>
+  ) 
 
   return (
       <div>
         <Navbar/>
 
         <Routes>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/signup' element={<SignUpPage/>}/>
-          <Route path='/login' element={<LogInPage/>}/>
-          <Route path='/profile' element={<ProfilePage/>}/>
+          <Route path='/' element={authUser ? <HomePage/> : <Navigate to='/login'/>}/>
+          <Route path='/profile' element={authUser ? <ProfilePage/> : <Navigate to='/login'/>}/>
+          <Route path='/signup' element={!authUser ? <SignUpPage/> : <Navigate to='/'/>}/>
+          <Route path='/login' element={!authUser ? <LogInPage/> : <Navigate to='/'/>}/>
           <Route path='/setting' element={<SettingPage/>}/>
         </Routes>
       </div>
-  )
+  )   
 }
 
 export default App
