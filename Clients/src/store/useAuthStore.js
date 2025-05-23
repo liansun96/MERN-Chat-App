9704bf9e9adafca1b7462457259e0f383d6a8476
liansun96 <liansun0203@gmail.com>
2025-05-23 22:43:15 +0630

    update

--- Clients/src/store/useAuthStore.js
+++ Clients/src/store/useAuthStore.js
@@ -2,11 +2,11 @@ import {create} from 'zustand'
 import { axiosInstance } from '../lib/axios'
 
 export const useAuthStore = create((set) => ({
+    
     authUser : null ,
     isSigningUp : false , 
     isLoggingIn : false , 
     isUpdatingProfile : false ,
-
     isCheckingAuth : true,
     
     checkAuth : async () => {
